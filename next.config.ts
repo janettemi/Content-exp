00eb0f1304a37b9cf973c886b8b52962d873e986
Ogunnaike janet <ogunnaikejanettemitope@@gamil.com>
2026-04-03 10:51:25 -0700

    Fix hostname under config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-   images: {
-    domains: ["cdn.dummyjson.com"],
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+      },
+    ],
   },
 };
 
-module.exports = nextConfig;
 export default nextConfig;
