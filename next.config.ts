334e0b83ec9bc005a0edac3a1872082b6fc39be5
Ogunnaike janet <ogunnaikejanettemitope@@gamil.com>
2026-04-03 10:43:28 -0700

    fix bug under config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.dummyjson.com",
-      },
-    ],
-  },
+import type { NextConfig } from "next";
 
-  
+const nextConfig: NextConfig = {
+  /* config options here */
+   images: {
+    domains: ["cdn.dummyjson.com"],
+  },
 };
 
+module.exports = nextConfig;
 export default nextConfig;
